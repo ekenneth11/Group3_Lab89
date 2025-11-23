9401562d3a1834c9634055558bae24f4a1705964
Kenneth Bautista <kennethbautista1512@gmail.com>
2025-11-23 11:08:38 -0500

    updated the swimEntryTests

--- Group3_Lab89/SwimLibTests/SwimEntryTest.cs
+++ Group3_Lab89/SwimLibTests/SwimEntryTest.cs
@@ -1,11 +1,29 @@
+using SwimLib;
 namespace SwimLibTests;
 
 [TestClass]
 public class SwimEntryTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void SwimEntry_Constructor()
     {
-        //ss
+        Swimmer swimTest = new Swimmer(25, DateTime.Now.AddYears(-12), "Lion Cubs", Sex.Male);
+        SwimEvent swimEventTest = new SwimEvent(Stroke.Backstroke, 50, Gender.Male, 12);
+        SwimEntry swimEntryTest = new SwimEntry(swimTest, swimEventTest, new TimeSpan(0, 0, 1, 5, 500));
+
+        Assert.IsNotNull(swimEntryTest);
+        Assert.AreEqual(swimTest, swimEntryTest.Swimmer);
+        Assert.AreEqual(swimEventTest, swimEntryTest.Event);
+    }
+    [TestMethod]
+    public void SwimEntry_Properties_NoValues()
+    {
+        Swimmer swimTest = new Swimmer(25, DateTime.Now.AddYears(-12), "Lion Cubs", Sex.Male);
+        SwimEvent swimEventTest = new SwimEvent(Stroke.Backstroke, 50, Gender.Male, 12);
+        SwimEntry swimEntryTest = new SwimEntry(swimTest, swimEventTest, new TimeSpan(0, 0, 1, 5, 500));
+
+        Assert.AreEqual(TimeSpan.Zero, swimEntryTest.Result, "Result must be 00:00:00");
+        Assert.AreEqual((byte)0, swimEntryTest.HeatNumber, "Heat Number must be 0");
+        Assert.AreEqual((byte)0, swimEntryTest.LaneNumber, "Lane Number must be 0");
     }
 }
